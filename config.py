f0e33b4f659ceeaa86dc3bc218d1b08b910eb2b5
Alikhan <alikhanaitbayevpost@gmail.com>
2023-07-04 16:48:11 +0600

    database connection

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:qwerty@5436/smilecook'
+    SQLALCHEMY_TRACK_MODIFICATION = False
--- models/recipe.py
+++ models/recipe.py
@@ -1,3 +1,4 @@
+from extensions import db
 recipe_list = []
 def get_last_id():
     if recipe_list:
@@ -6,7 +7,20 @@ def get_last_id():
         return 1
     return last_recipe.id+1
 
-class Recipe:
+class Recipe(db.Model):
+    __tablename__='recipe'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),nullable=False)
+    description = db.Column(db.String(200))
+    num_of_servings = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    directions = db.Column(db.String(1000))
+    id_publish=db.Column(db.Boolean(),
+                         default=False)
+    created_at=db.Column(db.DateTime(), nullable=False,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(),nullable=False,server_default=db.func.now(),onupdate=db.func.now())
+    user_id = db.Column(db.Integer(),db.ForeignKey("user.id"))
+    
     def __init__(self, name, description, num_of_servings, cook_time, directions) -> None:
         self.id = get_last_id()
         self.name = name
