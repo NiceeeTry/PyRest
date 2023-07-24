b837dca9510cce50aa2835097d64500746dfc2ec
Alikhan <alikhanaitbayevpost@gmail.com>
2023-07-24 14:39:52 +0600

    mail token activation

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
-from flask import request
+from flask import request, url_for
 from flask_restful import Resource
 from http import HTTPStatus
-from utils import hash_password
+from utils import hash_password, generate_token, verify_token
 from models.user import User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from schemas.user import UserSchema
@@ -9,11 +9,13 @@ from schemas.recipe import RecipeSchema
 from webargs import fields
 from webargs.flaskparser import use_kwargs
 from models.recipe import Recipe
-
+from mailgun import MailgunApi
 
 user_schema = UserSchema()
 user_public_schema = UserSchema(exclude=('email',))
 recipe_list_schema = RecipeSchema(many=True)
+mailgun = MailgunApi(domain='sandbox9fffe912a8954c85a2a02c314af22279.mailgun.org',
+                     api_key='00e80b4c2cd48e64acc4608679e11ae1-c30053db-6f667482')
 
 class UserListResource(Resource):
     def post(self):
@@ -36,6 +38,12 @@ class UserListResource(Resource):
 
         user = User(**data)
         user.save()
+        token = generate_token(user.email, salt='activate')
+        subject = 'Please confirm your registration.'
+        link = url_for('useractivateresource', token=token, _external=True)
+        text = 'Hi, Thanks for using SmileCook! Please confirm your registration by clicking on the link: {}'.format(link)
+        mailgun.send_email(to=user.email, subject=subject, text=text)
+        
 
         return user_schema.dump(user), HTTPStatus.CREATED
         # username = json_data.get('username')
@@ -104,3 +112,20 @@ class UserRecipeListResource(Resource):
         print(visibility,'-----------------------',current_user, user.id)
         recipes = Recipe.get_all_by_user(user_id=user.id, visibility=visibility)
         return recipe_list_schema.dump(recipes), HTTPStatus.OK
+    
+class UserActivateResource(Resource):
+    def get(self, token):
+        email = verify_token(token, salt='activate')
+        if email is False:
+            return {'message':'Invalid token or token expired'}, HTTPStatus.BAD_REQUEST
+        
+        user = User.get_by_email(email=email)
+        if not user:
+            return {'message':'User not found'}, HTTPStatus.NOT_FOUND
+        
+        if user.is_active is True:
+            return {'message':'The user account is already activated'}, HTTPStatus.BAD_REQUEST
+        
+        user.is_active = True
+        user.save()
+        return {}, HTTPStatus.NO_CONTENT
